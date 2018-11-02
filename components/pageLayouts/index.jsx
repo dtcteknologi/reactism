2409bb51f483830bc9298e57891c627441c132a2
Gifa Eriyanto <gifa.eriyanto@gmail.com>
2018-11-02 22:52:17 +0800

    Remove loading before render

--- components/pageLayouts/index.jsx
+++ components/pageLayouts/index.jsx
@@ -1,29 +1,6 @@
-import { useState, useEffect } from 'react'
 import Seo from './partials/seo'
 
 const MainLayout = ({ ...props }) => {
-  const [ loading, setLoading ] = useState(true)
-
-  useEffect(() => {
-    setLoading(false)
-  })
-
-  const splashscreen = {
-    width: '100%',
-    height: '100vh',
-    position: 'fixed',
-    top: 0,
-    left: 0,
-    backgroundColor: '#fff',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    zIndex: 2,
-    visibility: `${ loading ? 'visible' : 'hidden' }`,
-    opacity: `${ loading ? 1 : 0 }`,
-    transition: `${ loading ? 'opacity .1s linear' : 'visibility 0s .1s, opacity .1s linear' }`
-  }
-
   return (
     <>
       <Seo title={ props.title }/>
