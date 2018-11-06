7ed46acfd83a9fef1d45f7fbe9ca5c79739ec5ee
Gifa Eriyanto <gifaeriyanto@ymail.com>
2018-11-06 20:16:48 +0800

    PropsTable example

--- components/elements/button/index.jsx
+++ components/elements/button/index.jsx
@@ -1,10 +1,11 @@
-import { css } from 'emotion'
+import PropTypes from 'prop-types'
+import { cx, css } from 'emotion'
 import { Background } from 'reactism/utils'
 
-const Button = ({ children, color, ...props }) => {
-  const style = css`
-    ${ Background(color) }
-    border: 1px solid rgba(0,0,0,.1);
+const Button = ({ children, transparent, color, style, ...props }) => {
+  const baseStyle = css`
+    ${ transparent ? 'background-color: transparent;' : Background(color) }
+    border: 1px solid ${ transparent ? 'transparent' : 'rgba(0,0,0,.1)' };
     padding: 10px 15px;
     font-size: 1rem;
     border-radius: 3px;
@@ -15,18 +16,29 @@ const Button = ({ children, color, ...props }) => {
     &:last-child {
       margin-right: 0;
     }
-		&:hover {
-			box-shadow: 0 5px 30px rgba(0,0,0,.1);
-		}
-		&:active {
-			transform: scale(.98);
-			box-shadow: 0 0 0 rgba(0,0,0,.1);
-		}
+    ${ !transparent ?
+      `&:hover {
+        box-shadow: 0 5px 30px rgba(0,0,0,.1);
+      }
+      &:active {
+        transform: scale(.98);
+        box-shadow: 0 0 0 rgba(0,0,0,.1);
+      }`
+      : null
+    }
   `
 
   return (
-    <button className={ style } color={ color } { ...props }>{ children }</button>
+    <button className={ cx(baseStyle, style) } color={ color } { ...props }>{ children }</button>
   )
+}
+
+Button.propTypes = {
+  color: PropTypes.string
+}
+
+Button.defaultProps = {
+  color: null
 }
 
 export default Button
